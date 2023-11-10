667c53a64eb4a53831062e60c6391f61972e08c3
MD. MOINUL ISLAM <118617222+MOINUL909@users.noreply.github.com>
2023-11-10 11:30:55 +0600

    Add files via upload

---
+++ 1011.c
@@ -0,0 +1,18 @@
+/*MOINUL*/
+#include <stdio.h>
+
+int main() {
+    int R;
+    double S,pi=3.14159;
+    scanf("%d",&R);
+    S=(4.0/3)*pi*R*R*R;
+    printf("VOLUME = %.3lf\n",S);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
